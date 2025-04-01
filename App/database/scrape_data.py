a88b679b7f295adbee5f346247f5fd316fd16727
Chong Lee <upgamesemail@gmail.com>
2025-04-01 16:30:17 +0300

    added a lot of functionality TODO: finnish getting the twins data

--- App/database/scrape_data.py
+++ App/database/scrape_data.py
@@ -1,5 +1,6 @@
 #WARNING: This script is not intended to be run directly. It is a module that is imported by other scripts.
 #Script runtime might take a while due to the delay between requests.
+
 import os
 import json
 import re
@@ -45,7 +46,7 @@ def get_data() -> tuple:
 
 # checks if the site cache exists and if it is up to date if not it loads the site again and saves it to the cache
 # returns the BeautifulSoup object of the site
-def __load_cached_site(filepath, url) -> BeautifulSoup:
+def __load_cached_site(filepath, url) -> tuple:
     # check if the site was cached today and if not load it again
     today = date.today().strftime("%Y-%m-%d")
     cached_today = False
@@ -56,17 +57,15 @@ def __load_cached_site(filepath, url) -> BeautifulSoup:
             timestamp = json.loads(file.read())
             # check if the site was updated today
             if timestamp == today:
-                print("---- Cache is up to date ----")
                 cached_today = True
     
     # check if the file exists
     if not os.path.exists(filepath) or not cached_today:
-        print("---- Loading site ----")
         # send a GET request to the wiki URL
         response = requests.get(url)
         # if the request was unsuccessful, return None
         if response.status_code != 200:
-            return None
+            return None, True, f'Error: Could not connect to site {url}'
         # save the response content to a file
         with open(filepath, 'wb') as file:
             file.write(response.content)
@@ -75,18 +74,21 @@ def __load_cached_site(filepath, url) -> BeautifulSoup:
             file.write(json.dumps(today))
          
         # return the BeautifulSoup object 
-        return BeautifulSoup(response.content, 'html.parser')
+        return BeautifulSoup(response.content, 'html.parser'), True,'---- New site cached ----'
     
-    print("---- Loading cached site ----")
     ## if the file exists, read the content and return the BeautifulSoup object
     with open(filepath, 'rb') as file:
         content = file.read()
-    return BeautifulSoup(content, 'html.parser')
+    return BeautifulSoup(content, 'html.parser'), False, '---- Cached site loaded ----'
     
  # scrapes the site and returns a list of generations and a list of holos
 def __scrape_site() -> tuple:
-    soup = __load_cached_site('site_cache/site.cache', wiki_url + members_url)
+    soup, requested, msg = __load_cached_site('site_cache/site.cache', wiki_url + members_url)
+    if requested:
+        sleep(request_delay)
+    print(msg)
     if soup is None:
+
         return [], [], False, "Error: could not load cached site or fetch new data"
     
     try:
@@ -94,7 +96,9 @@ def __scrape_site() -> tuple:
         if not success:
             return [], [], False, "Error: Could not parse data"
         
-        generations = __get_generation_data(generations)
+        generations, success = __get_generation_data(generations)
+        if not success:
+            return [], [], False, "Error: Could not load generation data"
         holos = __get_holo_data(holos)
         
     except Exception as e:
@@ -131,6 +135,9 @@ def __parse_initial_data(soup) -> tuple:
             name = cells[0].get_text(strip=True)
             debut = cells[1].get_text(strip=True)
             
+            if name == 'Shirakami Fubuki':
+                generation = 'hololive 1st Generation'
+            
             if not any(gen['name'] == generation for gen in generations):
                 gen = {
                     'name': generation,
@@ -151,31 +158,108 @@ def __parse_initial_data(soup) -> tuple:
                 'wiki_link': link,
                 'retired': False if len(cells) <= 4 else True
             }
-            holos.append(holo)
+            
             # in case of fuwamoco add mococo too
             if name == 'Fuwawa Abyssgard':
                 mococo = {
                     'name': 'Mococo Abyssgard',
                     'debut': debut,
                     'generation': generation,
-                    'wiki_link': wiki_url + '/wiki/Mococo_Abyssgard',
-                    'retired': False
+                    'wiki_link': wiki_url + '/wiki/fuwamoco',
+                    'retired': False,
                 }
-                holos.append(mococo)   
+                holo['wiki_link'] = wiki_url + '/wiki/fuwamoco'
+                holos.append(holo)
+                holos.append(mococo)
+                continue
+                
+            holos.append(holo)
             
-    
+            
+            
     return generations, holos, True
 
-def __get_generation_data(generations) -> list:
+def __get_generation_data(generations) -> tuple:
     print("---- Loading generation data ----")
-    for i in tqdm(range(5), bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}'):
-        sleep(20)
-    return generations
+    for i in tqdm(range(len(generations)), bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}'):
+        soup, requested, msg = __load_cached_site(f'site_cache/generations/{generations[i]['name']}.cache', generations[i]['wiki_link'])
+        if soup is None:
+            print(msg)
+            return generations, False
+        overview = "\n" + soup.find('div', class_='mw-parser-output').find('p').get_text().rstrip()
+        overview = re.sub(r'\[.*?\]', '', overview)
+        generations[i]['overview'] = overview
+        if requested:
+            sleep(request_delay)
+    return generations, True
 
 def __get_holo_data(holos) -> list:
     print("---- Loading holo data ----")
-    for i in tqdm(range(100), bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}'):
-        sleep(20)
+    for i in tqdm(range(len(holos)), bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}'):
+        soup, requested, msg = __load_cached_site(f'site_cache/holos/{holos[i]['name']}.cache', holos[i]['wiki_link'])
+        if soup is None:
+            print(msg)
+            return holos
+        
+        '''
+            *JapName VARCHAR(64) NOT NULL,
+            *Height VARCHAR(128) NOT NULL,
+            *Birthday VARCHAR(128) NOT NULL,
+            Overview TEXT NOT NULL,
+            *ImageLink VARCHAR(128) NOT NULL,
+            YoutubeLink VARCHAR(128) NOT NULL,
+            TwitterLink VARCHAR(128) NOT NULL,
+        '''
+        
+        if not 'Abyssgard' in holos[i]['name']:
+            infobox = soup.find('table', class_='infobox')
+            rows = infobox.find_all('tr')
+        
+            img_link = rows[1].find('a', class_= 'mw-file-description').get('href')
+            img_link = wiki_url + img_link
+            
+            jp_name_row = infobox.find(lambda tag: tag.name == 'th' and 'Japanese Name' in tag.get_text())
+            jp_name_parent = jp_name_row.parent if jp_name_row else None
+            jp_name = jp_name_parent.find('td').get_text(strip=True) if jp_name_parent else ''
+            if holos[i]['name'] in ['AZKi']:
+                jp_name = holos[i]['name']
+            if jp_name == '':
+                print(f'\n{holos[i]['name']}: no Japanese name found')
+            
+            birthday_row = infobox.find(lambda tag: tag.name == 'a' and 'Birthday' in tag.get_text())
+            birthday_parent = birthday_row.parent.parent if birthday_row else None
+            birthday = birthday_parent.find('td').get_text(strip=True) if birthday_parent else ''
+            birthday = re.sub(r'\[.*?\]', '', birthday)
+            if birthday == '':
+                print(f'\n{holos[i]['name']}: no birthday found')
+            
+            height_row = infobox.find(lambda tag: tag.name == 'a' and 'Height' in tag.get_text())
+            height_parent = height_row.parent.parent if height_row else None
+            height = height_parent.find('td').get_text(strip=True) if height_parent else ''
+            height = re.sub(r'\[.*?\]', '', height)
+            height = re.sub(r'\(.*?\)', '', height)
+            if height == '':
+                print(f'\n{holos[i]['name']}: no height found')
+            
+            yt_link = infobox.find(lambda tag: tag.name == 'a' and 'YouTube' in tag.get_text())
+            yt_parent = yt_link.parent.parent if yt_link else None
+            yt_link = yt_parent.find('a', class_='external').get('href') if yt_parent else ''
+            if yt_link == '':
+                print(f'\n{holos[i]['name']}: no link found')
+        else:
+            twin_name = holos[i]['name'].replace(' Abyssgard', '')
+            
+            
+            
+
+        
+        
+    
+    
+        if requested and i != len(holos) - 1:
+            sleep(20)
+    for holo in holos:
+        print(holo['name'])        
     return holos
 
 
--- App/database/sql/create_holo.sql
+++ App/database/sql/create_holo.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS HOLO(
     HoloID INTEGER PRIMARY KEY AUTOINCREMENT,
+    GenerationID VARCHAR(64) NOT NULL,
     EngName VARCHAR(64) NOT NULL,
     JapName VARCHAR(64) NOT NULL,
-    GenerationID VARCHAR(64) NOT NULL,
     DebutDate Date NOT NULL,
-    Height INTEGER NOT NULL,
-    Birthday Date NOT NULL,
+    Height VARCHAR(128) NOT NULL,
+    Birthday VARCHAR(128) NOT NULL,
     Overview TEXT NOT NULL,
-    Personality TEXT NOT NULL,
     ImageLink VARCHAR(128) NOT NULL,
     YoutubeLink VARCHAR(128) NOT NULL,
     TwitterLink VARCHAR(128) NOT NULL,
     Retired BOOLEAN NOT NULL,
+    WikiLink VARCHAR(256) NOT NULL,
     FOREIGN KEY(GenerationID) REFERENCES GENERATION(GenerationID)
 )
