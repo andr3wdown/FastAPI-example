d8d37240bf76a6471e6ba92a85bb5465edb7e7b6
Chong Lee <upgamesemail@gmail.com>
2025-04-02 05:46:22 +0300

    finished scraping implementation

--- App/database/database_creation.py
+++ App/database/database_creation.py
@@ -1,43 +1,49 @@
+from scrape_data import get_data
 import sqlite3
 #connect to database
 connection = sqlite3.connect('holobase.db')
 cursor = connection.cursor()
 
-#read query from file
-def read_query_from_file(file_path):
-    with open(file_path, 'r') as file:
-        query = file.read()
-    return query
+
 
 #insert branches and generations to database
-def add_groups(groups, table_name):
-    sql_template = f'INSERT INTO {table_name}(EngName, JapName, DebutDate, Overview, ImageLink) VALUES(_en, _jn, _dd, _ov, _il)'
-    for group in groups:
-        sql_query = sql_template.replace('_en', group['EngName']).replace('_jn', group['JapName']).replace('_dd', group['DebutDate']).replace('_ov', group['Overview']).replace('_il', group['ImageLink'])
-        cursor.execute(sql_query)
-    connection.commit()
+def add_generations(generations, sql):
+    
+    
+    return sql
 
 #insert hololive members to database 
-def add_holos(holos):
-    sql_template = f'INSERT INTO HOLO(EngName, JapName, BranchID, GenerationID, DebutDate, Height, Birthday, Overview, Personality, ImageLink, YoutubeLink, TwitterLink) VALUES(_en, _jn, _bid, _gid, _dd, _ht, _bd, _ov, _per, _il, _yl, _tl)'
-    for holo in holos:
-        sql_query = sql_template.replace('_en', holo['EngName']).replace('_jn', holo['JapName']).replace('_bid', holo['BranchID']).replace('_gid', holo['GenerationID']).replace('_dd', holo['DebutDate']).replace('_ht', holo['Height']).replace('_bd', holo['Birthday']).replace('_ov', holo['Overview']).replace('_per', holo['Personality']).replace('_il', holo['ImageLink']).replace('_yl', holo['YoutubeLink']).replace('_tl', holo['TwitterLink'])
-        cursor.execute(sql_query)
-    connection.commit()
+def add_holos(holos, sql):
+    
+    return sql
     
 #create database tables
 def create_database():
-    #create tables
-    query = read_query_from_file('sql/create_branch.sql')
+    generations, holos, success = get_data()
+    
+    #create tables   
+    query = reqd_query_template('sql/create_generation.sql')
+    
     cursor.execute(query)
     
-    query = read_query_from_file('sql/create_generation.sql')
-    cursor.execute(query)
+    query = reqd_query_template('sql/create_holo.sql')
     
-    query = read_query_from_file('sql/create_holo.sql')
     cursor.execute(query)
     connection.commit()
+    
+#read query from file
+def reqd_query_template(file_path) -> str:
+    with open(file_path, 'r') as file:
+        query = file.read()
+    return query
     
 def close_connection():
     connection.close()
  
+
+
+if __name__ == "__main__":
+    create_database()
+    close_connection()
+    
+    
--- App/database/scrape_data.py
+++ App/database/scrape_data.py
@@ -48,7 +48,7 @@ def get_data() -> tuple:
 # returns the BeautifulSoup object of the site
 def __load_cached_site(filepath, url) -> tuple:
     # check if the site was cached today and if not load it again
-    today = date.today().strftime("%Y-%m-%d")
+    today = date.today().strftime("%Y-%m")
     cached_today = False
     # check if the timestamp file exists
     if os.path.exists(filepath+'.timestamp'):
@@ -56,7 +56,7 @@ def __load_cached_site(filepath, url) -> tuple:
         with open(filepath+'.timestamp', 'r') as file:
             timestamp = json.loads(file.read())
             # check if the site was updated today
-            if timestamp == today:
+            if today in timestamp:
                 cached_today = True
     
     # check if the file exists
@@ -88,7 +88,6 @@ def __scrape_site() -> tuple:
         sleep(request_delay)
     print(msg)
     if soup is None:
-
         return [], [], False, "Error: could not load cached site or fetch new data"
     
     try:
@@ -99,7 +98,10 @@ def __scrape_site() -> tuple:
         generations, success = __get_generation_data(generations)
         if not success:
             return [], [], False, "Error: Could not load generation data"
-        holos = __get_holo_data(holos)
+        
+        holos, success = __get_holo_data(holos)
+        if not success:
+            return [], [], False, "Error: Could not load holo data"
         
     except Exception as e:
         print(f"Error parsing data: {e}")
@@ -186,82 +188,119 @@ def __get_generation_data(generations) -> tuple:
         if soup is None:
             print(msg)
             return generations, False
-        overview = "\n" + soup.find('div', class_='mw-parser-output').find('p').get_text().rstrip()
+        overview = soup.find('div', class_='mw-parser-output').find('p').get_text().rstrip()
         overview = re.sub(r'\[.*?\]', '', overview)
         generations[i]['overview'] = overview
         if requested:
             sleep(request_delay)
     return generations, True
 
-def __get_holo_data(holos) -> list:
+def __get_holo_data(holos) -> tuple:
     print("---- Loading holo data ----")
     for i in tqdm(range(len(holos)), bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}'):
         soup, requested, msg = __load_cached_site(f'site_cache/holos/{holos[i]['name']}.cache', holos[i]['wiki_link'])
         if soup is None:
             print(msg)
-            return holos
+            return holos, False
         
-        '''
-            *JapName VARCHAR(64) NOT NULL,
-            *Height VARCHAR(128) NOT NULL,
-            *Birthday VARCHAR(128) NOT NULL,
-            Overview TEXT NOT NULL,
-            *ImageLink VARCHAR(128) NOT NULL,
-            YoutubeLink VARCHAR(128) NOT NULL,
-            TwitterLink VARCHAR(128) NOT NULL,
-        '''
+        infobox = soup.find('table', class_='infobox')
         
-        if not 'Abyssgard' in holos[i]['name']:
-            infobox = soup.find('table', class_='infobox')
-            rows = infobox.find_all('tr')
+        #handle the case of fuwamoco twins 
+        #get separate infoboxes from the same page
+        if 'Abyssgard' in holos[i]['name']:
+            twin_name = holos[i]['name']
+            infobox_row = soup.find(lambda tag: tag.name == 'th' and twin_name in tag.get_text())
+            infobox = infobox_row.parent.parent if infobox_row else None
         
-            img_link = rows[1].find('a', class_= 'mw-file-description').get('href')
-            img_link = wiki_url + img_link
+        rows = infobox.find_all('tr')
+    
+        img_link = rows[1].find('a', class_= 'mw-file-description').get('href')
+        img_link = wiki_url + img_link
+        
+        jp_name_row = infobox.find(lambda tag: tag.name == 'th' and 'Japanese Name' in tag.get_text())
+        jp_name_parent = jp_name_row.parent if jp_name_row else None
+        jp_name = jp_name_parent.find('td').get_text(strip=True) if jp_name_parent else ''
+        if holos[i]['name'] in ['AZKi']:
+            jp_name = holos[i]['name']
+        if jp_name == '':
+            jp_name = 'Unknown'
+            #print(f'\n{holos[i]['name']}: no Japanese name found')
+        
+        birthday_row = infobox.find(lambda tag: tag.name == 'a' and 'Birthday' in tag.get_text())
+        birthday_parent = birthday_row.parent.parent if birthday_row else None
+        birthday = birthday_parent.find('td').get_text(strip=True) if birthday_parent else ''
+        birthday = re.sub(r'\[.*?\]', '', birthday)
+        if birthday == '':
+            birthday = 'Unknown'
+            #print(f'\n{holos[i]['name']}: no birthday found')
+        
+        height_row = infobox.find(lambda tag: tag.name == 'a' and 'Height' in tag.get_text())
+        height_parent = height_row.parent.parent if height_row else None
+        height = height_parent.find('td').get_text(strip=True) if height_parent else ''
+        height = re.sub(r'\[.*?\]', '', height)
+        height = re.sub(r'\(.*?\)', '', height)
+        if height == '':
+            height = 'Unknown'
+            #print(f'\n{holos[i]['name']}: no height found')
+        
+        #get the right infobox for fuwamoco twins for their socials since they share accounts
+        if 'Abyssgard' in holos[i]['name']:
+            twin_name = holos[i]['name']
+            infobox_row = soup.find(lambda tag: tag.name == 'th' and 'FUWAMOCO' in tag.get_text())
+            infobox = infobox_row.parent.parent if infobox_row else None
+        
+        yt_link = infobox.find(lambda tag: tag.name == 'a' and 'YouTube' in tag.get_text())
+        yt_parent = yt_link.parent.parent if yt_link else None
+        yt_link = yt_parent.find('a', class_='external').get('href') if yt_parent else ''
+        if yt_link == '':
+            yt_link = 'Channel closed'
+            #print(f'\n{holos[i]['name']}: no link found')
             
-            jp_name_row = infobox.find(lambda tag: tag.name == 'th' and 'Japanese Name' in tag.get_text())
-            jp_name_parent = jp_name_row.parent if jp_name_row else None
-            jp_name = jp_name_parent.find('td').get_text(strip=True) if jp_name_parent else ''
-            if holos[i]['name'] in ['AZKi']:
-                jp_name = holos[i]['name']
-            if jp_name == '':
-                print(f'\n{holos[i]['name']}: no Japanese name found')
+        twt_link = infobox.find(lambda tag: tag.name == 'a' and 'Twitter' in tag.get_text())
+        twt_parent = twt_link.parent.parent if twt_link else None
+        twt_link = twt_parent.find('a', class_='external').get('href') if twt_parent else ''
+        if twt_link == '':
+            twt_link = 'Twitter closed'
+            #print(f'\n{holos[i]['name']}: no link found')
             
-            birthday_row = infobox.find(lambda tag: tag.name == 'a' and 'Birthday' in tag.get_text())
-            birthday_parent = birthday_row.parent.parent if birthday_row else None
-            birthday = birthday_parent.find('td').get_text(strip=True) if birthday_parent else ''
-            birthday = re.sub(r'\[.*?\]', '', birthday)
-            if birthday == '':
-                print(f'\n{holos[i]['name']}: no birthday found')
-            
-            height_row = infobox.find(lambda tag: tag.name == 'a' and 'Height' in tag.get_text())
-            height_parent = height_row.parent.parent if height_row else None
-            height = height_parent.find('td').get_text(strip=True) if height_parent else ''
-            height = re.sub(r'\[.*?\]', '', height)
-            height = re.sub(r'\(.*?\)', '', height)
-            if height == '':
-                print(f'\n{holos[i]['name']}: no height found')
-            
-            yt_link = infobox.find(lambda tag: tag.name == 'a' and 'YouTube' in tag.get_text())
-            yt_parent = yt_link.parent.parent if yt_link else None
-            yt_link = yt_parent.find('a', class_='external').get('href') if yt_parent else ''
-            if yt_link == '':
-                print(f'\n{holos[i]['name']}: no link found')
-        else:
-            twin_name = holos[i]['name'].replace(' Abyssgard', '')
-            #TODO: HANDLE TWINS
-            
-            
-            
+        
 
+        overview_heading = soup.find('span', id='Overview').find_parent('h2')
+        overview_paragraphs = []
         
+        for element in overview_heading.find_next_siblings():
+            if element.name == 'h2':
+                break
+            if element.name == 'p':
+                overview_paragraphs.append(element.get_text(strip=True))
+        if len(overview_paragraphs) == 0:
+            bio_heading = soup.find('span', id='Official_Bio').find_parent('h2')
+            for element in bio_heading.find_next_siblings():
+                if element.name == 'h2':
+                    break
+                if element.name == 'p':
+                    overview_paragraphs.append(element.get_text(strip=True))
         
-    
-    
+        if len(overview_paragraphs) == 0:
+            overview_paragraphs.append('No overview found')
+        overview = overview_paragraphs[0] + ('\n' + overview_paragraphs[1] if len(overview_paragraphs) > 1 else '')
+        overview = re.sub(r'\[.*?\]', '', overview)   
+        if(overview == ''):
+            overview = 'No overview found'
+            #print(f'\n{holos[i]['name']}: no overview found')
+
+        holos[i]['jp_name'] = jp_name
+        holos[i]['height'] = height
+        holos[i]['birthday'] = birthday
+        holos[i]['overview'] = overview
+        holos[i]['img_link'] = img_link
+        holos[i]['yt_link'] = yt_link
+        holos[i]['twt_link'] = twt_link
+        
         if requested and i != len(holos) - 1:
             sleep(20)
-    for holo in holos:
-        print(holo['name'])        
-    return holos
+                   
+    return holos, True
 
 
 #determines the branch of the generation based on the name
