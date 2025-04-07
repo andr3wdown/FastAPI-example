8cd5c53724b097ebb52d57b9666ace7cd3200735
Chong Lee <upgamesemail@gmail.com>
2025-04-07 22:17:56 +0300

    created get_holo for getting info on a hololive member

--- App/database/database_access.py
+++ App/database/database_access.py
@@ -2,3 +2,33 @@ import sqlite3
 
 connection = sqlite3.connect('holobase.db')
 cursor = connection.cursor()
+
+column_names = { }
+
+def get_holo(name):
+    if 'holo' not in column_names:
+        column_names['holo'] = __get_column_names('HOLO')
+    
+    name = __sanitize_input(name)
+    name = name.capitalize()
+    name = f"%{name}%"
+    
+    sql = '''SELECT * FROM HOLO WHERE EngName LIKE ?'''
+    cursor.execute(sql, (name,))
+    holo = cursor.fetchone()
+    return holo
+
+# Get the column names of a table in the database using PRAGMA table_info
+def __get_column_names(table_name):
+    
+    sql = f"PRAGMA table_info({table_name})"
+    cursor.execute(sql)
+    columns = [column[1] for column in cursor.fetchall()]
+    return columns
+
+# Remove any special characters or SQL injection attempts
+# this isn't strictly necessary since I'm using parameterized queries and sqlite3 already escapes inputs, 
+# but it's a good practice to sanitize inputs anyway to prevent any potential risks
+def __sanitize_input(input_string):
+    sanitized_string = ''.join(e for e in input_string if e.isalnum() or e.isspace())
+    return sanitized_string
--- App/main.py
+++ App/main.py
@@ -29,7 +29,7 @@ async def get_api_key(api_key: str = Security(api_key_header_auth)):
     return api_key
 
 
-
+#test endpoint to check if the API key is valid
 @app.get("/test")
 async def read_root(api_key: str = Security(get_api_key)):
     return {
@@ -37,6 +37,7 @@ async def read_root(api_key: str = Security(get_api_key)):
             "message": "API key is valid"
         }
     
+
 @app.get("/holos/{name}")
 async def get_holos(api_key: str = Security(get_api_key)):
     return {
